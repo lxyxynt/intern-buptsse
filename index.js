9497ebd38481dec01bb04c0550f1d817a18d949a
trinity <imandry.c@gmail.com>
2013-06-01 13:38:19 +0800

    change the code for testable server

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
-var server = require('./lib/server.js');
-
-server.listen(10920);
-console.log('Listen on 10920.');
+var db = require('./lib/db');
+db.connect("mongodb://localhost/intern-buptsse");
+db.P(function () { 
+    var server = require('./lib/server.js');
+    server.listen(10920);
+    console.log('Listen on 10920.');
+})();
 
