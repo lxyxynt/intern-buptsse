7c78c64637c0309d5cb6148f41ccd1a1b5a91d40
trinity <imandry.c@gmail.com>
2013-06-01 13:31:20 +0800

    add client login logic demo code

--- www/asset/js/login.js
+++ www/asset/js/login.js
@@ -4,15 +4,27 @@
     $('#content').children().remove();
 
     $.get('/ui/login.js', function (code) {
-        $view = $(eval(code)());
+        $view = $(eval(code)(data));
         getView($view);
     }); 
 
     var getView = function ($view) {
         $view.children('form').submit(function () {
-            data.role = $('.username input').val();
-            console.log(data.role);
-            window.location.hash = "#dashboard";
+            data.username = $('.username input').val();
+            data.password = $('.password input').val();
+            $.post('/login', {
+                username: data.username,
+                password: data.password
+            }, function (user) {
+                data.role = user.role;
+
+                // Other operations
+                window.location.hash = "#dashboard";
+            })
+            .fail(function () {
+                // TODO Improve this implementation
+                window.alert('错误的用户名或者密码');
+            });
             return false;
         });
         $view.appendTo("#content");
